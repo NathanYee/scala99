c5190eeb99652b09b806f6e844fb21a5fff30a40
Nathan Yee <nathaniel.yee@students.olin.edu>
2018-04-23 12:17:59 -0400

    did P01

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,11 +1,14 @@
 object P01 {
-  def last[A](xs: List[A]): A = xs match {
+  def last[A](l: List[A]): A = l match {
     case h :: Nil => h
-    case h :: tail => last(tail)
+    case _ :: tail => last(tail)
     case _ => throw new NoSuchElementException
   }
 
+  def last2[A](l: List[A]): A = l.last
+
   def main(args: Array[String]): Unit = {
     println(last(List(1, 1, 2, 3, 5, 8)))
+    println(last2(List(1, 1, 2, 3, 5, 8)))
   }
 }
